59660125c8cf3ada5265076daa981e5c6c80d587
cp-winslow <49729540+cp-winslow@users.noreply.github.com>
2019-07-09 09:36:04 -0500

    added homework.js. Still working on homework.

---
+++ Class_8/homework.js
@@ -0,0 +1,11 @@
+/*Create an object that models sample.html
+(i.e. the object should contain all the details
+about the html, tags, attributes, content and etc.).*/
+
+(function myFunction() {
+
+  const sampleHTML = {
+
+  }
+
+}) ();
