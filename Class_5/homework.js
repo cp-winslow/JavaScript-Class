48d1aef2872c7ba50640a16a65652701713ae88f
cp-winslow <49729540+cp-winslow@users.noreply.github.com>
2019-07-10 11:44:12 -0500

    Update homework.js
    Not finished with homework.

--- Class_5/homework.js
+++ Class_5/homework.js
@@ -1,15 +1,22 @@
-//Define a function charCount that takes a string as an argument and returns an object containing character count of letters.
+/*Define a function charCount that takes a string as an argument
+and returns an object containing character count of letters.*/
 
-function charCount(string) {
-  this.str = string;
-  this.char = function () {
+(function () {
 
+  function charCount(string) {
+    this.string = string;
+    this.result = function (result) {
+      this.array = this.string.split("");
+    };
+    this.ltrCount = ;
+    this.splCount = ;
   };
 
-};
+  const result = charCount("Pack my box with five dozen liquor jugs.");
+  console.log(charCount.string);
+  const aCount = result.a;
+  const spaceCount = result[' '];
+  console.log(aCount); // Should output 1
+  console.log(spaceCount); // Should output 7
 
-const result = charCount("Pack my box with five dozen liquor jugs.");
-const aCount = result.a;
-const spaceCount = result[' '];
-console.log(aCount); // Should output 1
-console.log(spaceCount); // Should output 7
+}) ();
