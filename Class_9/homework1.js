659d7a4968dc990b093c9f10081ef80b7acc45c7
cp-winslow <49729540+cp-winslow@users.noreply.github.com>
2019-07-15 08:29:59 -0500

    Update homework1.js

--- Class_9/homework1.js
+++ Class_9/homework1.js
@@ -10,10 +10,27 @@
   const currentDiv = document.querySelectorAll("figure");
 
   for (let i = 0; i < currentDiv.length; i++) {
-    const altText = currentDiv[i].alt;
+    const altText = currentDiv[i].firstElementChild.alt;
     const newFigCaption = document.createElement("figcaption");
     const newFigText = document.createTextNode(altText);
     newFigCaption.appendChild(newFigText);
   }
 
 }) ();
+
+/*Add a script to gallery.html that [sets]
+(https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute)
+title attribute for each image with the value matching its alt text.
+*/
+  (function () {
+    const imgs = document.querySelectorAll("img");
+
+    for (let photo of imgs) {
+      photo.title = photo.alt;
+    }
+
+    for (let i = 0; i < imgs.length; i++) {
+      const altText = imgs[i].alt;
+      imgs[i].setAttribute("style", "border: solid red 5px");
+    }
+  })();
