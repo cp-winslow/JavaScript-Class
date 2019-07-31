03e361ce945df333b81b41c5d26273da2166ed46
cp-winslow <49729540+cp-winslow@users.noreply.github.com>
2019-07-31 10:01:01 -0500

    Updated homework.js

--- Class_5/homework.js
+++ Class_5/homework.js
@@ -4,16 +4,20 @@ and returns an object containing character count of letters.*/
 (function () {
 
   function charCount(string) {
-    this.string = string;
-    this.result = function (result) {
-      this.array = this.string.split("");
-    };
-    this.ltrCount = ;
-    this.splCount = ;
-  };
+    const charCountLen = string.length;
+    const charCountObject = {};
+    for (let i = 0; i < charCountLen; i++) {
+      let current = string.charAt(i);
+      if (charCountObject[current]) {
+        charCountObject[current] = charCountObject[current] + 1;
+      } else {
+        charCountObject[current] = 1;
+      }
+    }
+    return charCountObject;
+  }
 
   const result = charCount("Pack my box with five dozen liquor jugs.");
-  console.log(charCount.string);
   const aCount = result.a;
   const spaceCount = result[' '];
   console.log(aCount); // Should output 1
